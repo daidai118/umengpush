96f4ebdaeebe970757e986782781c10e41c00a59
wangsheng <13673811@qq.com>
2016-10-10 17:10:51 +0800

    init

---
+++ Umeng.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: daidai
+ * Date: 2016/10/10
+ * Time: 下午5:02
+ */
+
+use yii\base\Component;
+
+class Umeng extends Component
+{
+	public $api_uri_prefix = 'http://msg.umeng.com/';
+	public $appkey;
+	public $app_master_secret;
+	//客户端适配器(目前使用yii2-client)
+	public $client;
+}
